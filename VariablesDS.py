4c0600ef35eae1fa63f7aed76b532503afc68f8b
SlypeeDort <morozovr4368@mail.ru>
2024-08-06 13:19:02 +0500

    Add files via upload

---
+++ VariablesDS.py
@@ -0,0 +1,7 @@
+The_number_of_completed_DZ = 12
+Number_of_hours_spent = 1.5
+Course_name = 'Python'
+Time_for_one_task = Number_of_hours_spent / The_number_of_completed_DZ
+print('Курс: ' + Course_name + ', Всего задач:  ' + str(Time_for_one_task) +
+      ', Затрачено часов: ' + str(Number_of_hours_spent) + ', Cреднее время выполнения: '
+      + str(Number_of_hours_spent))
